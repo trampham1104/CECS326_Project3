80ccae0d0f700534fbc47003be7600ff6692fdf2
iamkellpham <Bich-Tram.Pham01@student.csulb.edu>
2022-04-07 20:14:35 -0700

    Starting

--- RoadController.java
+++ RoadController.java
@@ -4,9 +4,64 @@
  *
  */
 
- 
-public class RoadController
-{  
-   //Your code here
+import java.util.concurrent.Semaphore;
+
+
+public class RoadController{  
+   // Creating a road which 1 person crossing max
+   static Semaphore road = new Semaphore(1);
+   String name = "";
+
+   RoadController(East_village east){
+      this.name = "East Villager";
+   }
+
+   RoadController(West_village west){
+      this.name = "West Villager";
+   }
+
+   public void run(){
+
+      try {
+         System.out.println(name + " : is trying to cross the road...");
+         System.out.println("Is road available: " + road.availablePermits());
+
+         road.acquire();
+         System.out.println(name + " : can cross the road!");
+
+         try {
+            System.out.println(name + " : is crossing the road");
+
+            //sleep for 1 seconds as in walking
+            Thread.sleep(1000);
+
+         
+      } finally {
+
+         //Realeasing after successful crossing the road
+         System.out.println(name + " : finish crossing the road...");
+         road.release();
+         System.out.println("Is road available: " + road.availablePermits());
+        }
+
+      } catch (Exception e){
+         e.printStackTrace();
+        }
+      }
+
+   
+   public static void main(String[] a){
+      System.out.println("Is road available: " + road.availablePermits());
+
+      // Create villager
+      East_village east_villager = new East_village("East Villager");
+      West_village west_villager = new West_village("West Villager");
+
+
+      //RoadController t1 = new RoadController(east_villager);
+      east_villager.start();
+      west_villager.start();
+   }
+
 
 }
--- West_village.java
+++ West_village.java
@@ -1,7 +1,33 @@
 
-public class West_village 
+import java.util.Random;
+
+public class West_village extends Thread
 {
 
-   //Your code here
+   String name ="";
+   int bound = 3;
+
+   West_village(String name) {
+      this.name = name;
+   }
+
+   public String action(){
+      Random rand = new Random();
+      String action = "";
+      int int_random = rand.nextInt(bound);
+
+      switch (int_random){
+         case 0:
+            action = name + " is eating a double double";
+
+         case 1:
+            action = name + " is eating some curly fries";
+
+         case 2:
+            action = name + " is drinking their milkshake";
+      }
+
+      return action;
+   }
    
 }
